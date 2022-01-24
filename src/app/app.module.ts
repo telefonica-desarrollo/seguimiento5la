daf906aff93b53c63ab54cd6c0f0c8f6687464a0
Jesus Arath <jesus.flores.p19@gmail.com>
2022-01-24 16:35:02 -0600

    Creamos proyecto, con algunas vistas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidenavComponent } from './componentes/shared/sidenav/sidenav.component';
+import { TituloComponent } from './componentes/shared/titulo/titulo.component';
+import { NuevosRegistrosComponent } from './componentes/nuevos-registros/nuevos-registros.component';
+import { SeguimientoComponent } from './componentes/seguimiento/seguimiento.component';
+import { FinalizadosComponent } from './componentes/finalizados/finalizados.component';
+import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
+import { CargaComponent } from './componentes/carga/carga.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    TituloComponent,
+    NuevosRegistrosComponent,
+    SeguimientoComponent,
+    FinalizadosComponent,
+    ConfiguracionComponent,
+    CargaComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componentes/nuevos-registros/nuevos-registros.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nuevos-registros',
+  templateUrl: './nuevos-registros.component.html',
+  styleUrls: ['./nuevos-registros.component.css']
+})
+export class NuevosRegistrosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
